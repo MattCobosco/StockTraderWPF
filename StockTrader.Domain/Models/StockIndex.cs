a1d16233dd92f1729cd7b6641d5b8807bdb0abff
MattCobosco <mateuszkalucki@gmail.com>
2022-05-25 21:22:33 +0200

    Stock API data model

---
+++ StockTrader.Domain/Models/StockIndex.cs
@@ -0,0 +1,15 @@
+﻿namespace StockTrader.Domain.Models
+{
+    public enum StockIndexType
+    {
+        DowJones,
+        NASDAQ,
+        SP500
+    }
+    public class StockIndex
+    {
+        public string Price { get; set; }
+        public double Changes { get; set; }
+        public StockIndexType Type { get; set; }
+    }
+}
