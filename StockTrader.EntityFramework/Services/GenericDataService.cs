b0811cdf2663852dc538f76c7568c2ea23aeb836
MattCobosco <mateuszkalucki@gmail.com>
2022-05-25 13:36:49 +0200

    Generic CRUD Dataservice

---
+++ StockTrader.EntityFramework/Services/GenericDataService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockTrader.Domain.Models;
+using StockTrader.Domain.Services;
+
+namespace StockTrader.EntityFramework.Services
+{
+    public class GenericDataService<T> : IDataService<T> where T : DomainObject
+    {
+        private readonly StockTraderDbContextFactory _contextFactory;
+
+        public GenericDataService(StockTraderDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                var createdEntity = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdEntity.Entity;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e)=>e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+        public async Task<T> Get(int id)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+
+                return entity;
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                IEnumerable<T> entities = await context.Set<T>().ToListAsync();
+
+                return entities;
+            }
+        }
+
+        public async Task<T> Update(int id, T entity)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+    }
+}
