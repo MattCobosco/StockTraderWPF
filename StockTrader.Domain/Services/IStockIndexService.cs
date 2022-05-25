529c171b394cd05ad1d9f28a240470924b99b843
MattCobosco <mateuszkalucki@gmail.com>
2022-05-25 21:22:51 +0200

    service for getting the stock indexes from API

---
+++ StockTrader.Domain/Services/IStockIndexService.cs
@@ -0,0 +1,9 @@
+﻿using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services
+{
+    public interface IStockIndexService
+    {
+        Task<StockIndex> GetStockIndex(StockIndexType indexType); 
+    }
+}
