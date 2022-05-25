e69d3044bf266a2597f9eeab1e3f504d77b2b932
MattCobosco <mateuszkalucki@gmail.com>
2022-05-25 12:52:28 +0200

    DB Creation

---
+++ StockTrader.EntityFramework/StockTraderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockTrader.Domain.Models;
+
+namespace StockTrader.EntityFramework
+{
+    public class StockTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>()
+                .OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(local); Database=StockTraderDB; Trusted_Connection=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        } 
+    }
+}
