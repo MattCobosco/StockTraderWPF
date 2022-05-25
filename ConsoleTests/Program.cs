5e8db4e91a8bc2006f9f237f1a55f673e939f08c
MattCobosco <mateuszkalucki@gmail.com>
2022-05-25 13:54:20 +0200

    CRUD console tests

---
+++ ConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StockTrader.Domain.Models;
+using StockTrader.Domain.Services;
+using StockTrader.EntityFramework;
+using StockTrader.EntityFramework.Services;
+
+IDataService<User> userService = new GenericDataService<User>(new StockTraderDbContextFactory());
+Console.WriteLine(userService.Delete(3).Result);
+
+
+
+
