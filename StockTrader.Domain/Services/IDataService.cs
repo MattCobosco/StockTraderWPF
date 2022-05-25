738b3f5315e4b31b142e9c7b27e6327419a05312
MattCobosco <mateuszkalucki@gmail.com>
2022-05-25 12:32:12 +0200

    IDataService for generic CRUD

---
+++ StockTrader.Domain/Services/IDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace StockTrader.Domain.Services
+{
+    public interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> Get(int id);
+
+        Task<T> Create(T entity);
+
+        Task<T> Update(int id, T entity);
+
+        Task<bool> Delete(int id);
+    }
+}
