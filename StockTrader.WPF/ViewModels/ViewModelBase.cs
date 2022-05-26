498fdb368945e560555ea99eed88803997f96778
MattCobosco <mateuszkalucki@gmail.com>
2022-05-26 19:43:55 +0200

    ObservableObject inheritance

--- StockTrader.WPF/ViewModels/ViewModelBase.cs
+++ StockTrader.WPF/ViewModels/ViewModelBase.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StockTrader.WPF.Models;
 
 namespace StockTrader.WPF.ViewModels
 {
-    public class ViewModelBase
+    public class ViewModelBase : ObservableObject
     {
+
     }
 }
