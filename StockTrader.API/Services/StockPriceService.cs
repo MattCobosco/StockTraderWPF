9de1865fd8a6a7046ea3386bcbdbefdc64104241
MattCobosco <mateuszkalucki@gmail.com>
2022-05-26 20:54:45 +0200

    Service for fetching stock prices

---
+++ StockTrader.API/Services/StockPriceService.cs
@@ -0,0 +1,34 @@
+﻿
+using Newtonsoft.Json.Linq;
+using StockTrader.Domain.Models;
+using StockTrader.Domain.Services;
+using StockTrader.Domain.Exceptions;
+
+namespace StockTrader.YahooFinanceAPI.Services
+{
+    public class StockPriceService : IStockPriceService
+    {
+        private string _ApiKey = "TWUN8RpX1i8iki66peGve1IZbjFI2kcSaAyJ3r6i";
+
+        public async Task<double> GetStockPrice(string symbol)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string uri = "https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=" + symbol;
+                client.DefaultRequestHeaders.Add("accept", "application/json");
+                client.DefaultRequestHeaders.Add("X-API-KEY", _ApiKey);
+                HttpResponseMessage response = await client.GetAsync(uri);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                string regularMarketPrice = JObject.Parse(jsonResponse)["quoteResponse"]["result"][0]["regularMarketPrice"].ToString();
+                double price = Convert.ToDouble(regularMarketPrice);
+
+                if(price == 0)
+                {
+                    throw new InvalidSymbolException(symbol);
+                }
+                return price;
+            }
+        }
+    }
+}
---
+++ StockTrader.Domain/Services/IStockPriceService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader.Domain.Services
+{
+    public interface IStockPriceService
+    {
+        Task<double> GetStockPrice(string symbol);
+    }
+}
