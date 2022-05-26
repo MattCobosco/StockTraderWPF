64fd61b4d2863368fad8bb89cf54fed6de730e5f
MattCobosco <mateuszkalucki@gmail.com>
2022-05-26 20:06:17 +0200

    loads Home View on startup

--- StockTrader.WPF/ViewModels/MainViewModel.cs
+++ StockTrader.WPF/ViewModels/MainViewModel.cs
@@ -5,5 +5,10 @@ namespace StockTrader.WPF.ViewModels
     public class MainViewModel : ViewModelBase
     {
         public INavigator Navigator { get; set; } = new Navigator();
+
+        public MainViewModel()
+        {
+            Navigator.UpdateCurrentViewModelCommand.Execute(ViewType.Home);
+        }
     }
 }
