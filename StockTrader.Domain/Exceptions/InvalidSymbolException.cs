f30ade58374cbb7a4c7a15cfd983cda80bcabfb5
MattCobosco <mateuszkalucki@gmail.com>
2022-05-26 20:54:03 +0200

    custom exception for invalid stock symbol

---
+++ StockTrader.Domain/Exceptions/InvalidSymbolException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader.Domain.Exceptions
+{
+    public class InvalidSymbolException : Exception
+    {
+        public string Symbol { get; set; }
+
+        public InvalidSymbolException(string symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public InvalidSymbolException(string symbol, string? message) : base(message)
+        {
+            Symbol = symbol;
+        }
+
+        public InvalidSymbolException(string symbol, string? message, Exception? innerException) : base(message, innerException)
+        {
+            Symbol = symbol;
+        }
+
+        protected InvalidSymbolException(string symbol, SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            Symbol = symbol;
+        }
+    }
+}
