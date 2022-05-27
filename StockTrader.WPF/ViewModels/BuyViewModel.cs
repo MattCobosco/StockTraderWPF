5411e104c2fa7d17f493a47c4d57f8c4bd785881
MattCobosco <mateuszkalucki@gmail.com>
2022-05-27 22:59:00 +0200

    Search for BuyView, SearchSymbolCommand

--- StockTrader.WPF/ViewModels/BuyViewModel.cs
+++ StockTrader.WPF/ViewModels/BuyViewModel.cs
@@ -1,7 +1,45 @@
-﻿namespace StockTrader.WPF.ViewModels
+﻿using StockTrader.Domain.Services;
+using StockTrader.WPF.Commands;
+using System.Windows.Input;
+
+namespace StockTrader.WPF.ViewModels
 {
     public class BuyViewModel : ViewModelBase
     {
+        private string _symbol;
+        public string Symbol
+        {
+            get 
+            { 
+                return _symbol; 
+            }
+            set 
+            {
+                _symbol = value;
+                OnPropertyChanged(nameof(Symbol));
+            }
+        }
 
+
+        private double _stockPrice;
+        public double StockPrice
+        {
+            get
+            {
+                return _stockPrice;
+            }
+            set
+            {
+                _stockPrice = value;
+                OnPropertyChanged(nameof(StockPrice));
+            }
+        }
+
+        public ICommand SearchSymbolCommand { get; set; }
+
+        public BuyViewModel(IStockPriceService stockPriceService)
+        {
+            SearchSymbolCommand = new SearchSymbolCommand(this, stockPriceService);
+        }
     }
 }
