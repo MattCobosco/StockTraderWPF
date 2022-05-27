ad08653a89ca16aaef03ce539bc0742cec67d2be
MattCobosco <mateuszkalucki@gmail.com>
2022-05-27 21:07:50 +0200

    service to enable buying stocks

---
+++ StockTrader.Domain/Services/TransactionServices/BuyStockService.cs
@@ -0,0 +1,48 @@
+﻿using StockTrader.Domain.Exceptions;
+using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services.TransactionServices
+{
+    public class BuyStockService : IBuyStockService
+    {
+        private readonly IStockPriceService _stockPriceService;
+        private readonly IDataService<Account> _accountService;
+
+        public BuyStockService(IStockPriceService stockPriceService, IDataService<Account> accountService)
+        {
+            _stockPriceService = stockPriceService;
+            _accountService = accountService;
+        }
+
+        public async Task<Account> BuyStock(Account buyer, string stockSymbol, int shareAmount)
+        {
+            double stockPrice = await _stockPriceService.GetStockPrice(stockSymbol);
+            double transactionPrice = shareAmount * stockPrice;
+
+            if (stockPrice * shareAmount > buyer.Balance)
+            {
+                throw new InsufficientFundsException(buyer.Balance, transactionPrice);
+            }
+
+            AssetTransaction transaction = new AssetTransaction()
+            {
+                Account = buyer,
+                Stock = new Stock()
+                {
+                    PricePerShare = stockPrice,
+                    Symbol = stockSymbol,
+                },
+                DateProcessed = DateTime.Now,
+                ShareAmount = shareAmount,
+                IsBuy = true
+            };
+
+            buyer.AssetTransactions.Add(transaction);
+            buyer.Balance -= transactionPrice;
+
+            await _accountService.Update(buyer.Id, buyer);
+
+            return buyer;
+        }
+    }
+}
---
+++ StockTrader.Domain/Services/TransactionServices/IBuyStockService.cs
@@ -0,0 +1,9 @@
+﻿using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services.TransactionServices
+{
+    public interface IBuyStockService
+    {
+        Task<Account> BuyStock(Account buyer, string stockSymbol, int shareAmount);
+    }
+}
