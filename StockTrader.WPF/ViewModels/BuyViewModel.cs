b7bb0a084b7880168d8fe8049340f458283c82b5
MattCobosco <mateuszkalucki@gmail.com>
2022-05-27 22:27:32 +0200

    cleanup

--- StockTrader.WPF/ViewModels/BuyViewModel.cs
+++ StockTrader.WPF/ViewModels/BuyViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StockTrader.WPF.ViewModels
+﻿namespace StockTrader.WPF.ViewModels
 {
     public class BuyViewModel : ViewModelBase
     {
+
     }
 }
