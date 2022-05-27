1d9bb3aa9b3bae306da4bdcf3033d019966b3088
MattCobosco <mateuszkalucki@gmail.com>
2022-05-27 21:36:17 +0200

    Account Data service to get Accounts including AssetTransactions

---
+++ StockTrader.EntityFramework/Services/AccountDataService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using StockTrader.Domain.Models;
+using StockTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader.EntityFramework.Services
+{
+    public class AccountDataService : IDataService<Account>
+    {
+        private readonly StockTraderDbContextFactory _contextFactory;
+
+        public AccountDataService(StockTraderDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<Account> Create(Account entity)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                var createdEntity = await context.Set<Account>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdEntity.Entity;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                Account entity = await context.Set<Account>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<Account>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+        public async Task<Account> Get(int id)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                Account entity = await context.Accounts.Include(a=>a.AssetTransactions).FirstOrDefaultAsync((e) => e.Id == id);
+
+                return entity;
+            }
+        }
+
+        public async Task<IEnumerable<Account>> GetAll()
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                IEnumerable<Account> entities = await context.Accounts.Include(a => a.AssetTransactions).ToListAsync();
+
+                return entities;
+            }
+        }
+
+        public async Task<Account> Update(int id, Account entity)
+        {
+            using (StockTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+                context.Set<Account>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+    }
+}
