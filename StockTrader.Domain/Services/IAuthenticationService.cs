900fc9f9ba4c93fc498049ad67078a439a674d31
MattCobosco <mateuszkalucki@gmail.com>
2022-05-28 14:33:54 +0200

    IAuthenticationService for user authentication

---
+++ StockTrader.Domain/Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Register(string email, string username, string password, string confirmPassword);
+        Task<Account> Login(string username, string password);
+    }
+}
