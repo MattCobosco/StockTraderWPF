addf136de83cf1fe2636171bc841ed23b74dc85c
MattCobosco <mateuszkalucki@gmail.com>
2022-05-28 15:34:44 +0200

    custom exception for when user provides invalid password

---
+++ StockTrader.Domain/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,26 @@
+﻿namespace StockTrader.Domain.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public InvalidPasswordException(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public InvalidPasswordException(string username, string password, string? message) : base(message)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public InvalidPasswordException(string username, string password, string? message, Exception? innerException) : base(message, innerException)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
