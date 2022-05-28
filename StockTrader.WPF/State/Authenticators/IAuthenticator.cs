b9aac5cdbb110ed502126375cec7bf59cb29c64a
MattCobosco <mateuszkalucki@gmail.com>
2022-05-28 17:33:24 +0200

    interface for authentication

---
+++ StockTrader.WPF/State/Authenticators/IAuthenticator.cs
@@ -0,0 +1,16 @@
+﻿using StockTrader.Domain.Models;
+using StockTrader.Domain.Services;
+using System.Threading.Tasks;
+
+namespace StockTrader.WPF.State.Authenticators
+{
+    public interface IAuthenticator
+    {
+        User CurrentUser { get; }
+        bool IsLoggedIn { get; }
+
+        Task<RegistrationResult> Register(string email, string username, string password, string confirmPassword);
+        Task<bool> Login(string username, string password);
+        void Logout();
+    }
+}
