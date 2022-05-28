4013864c3392c2b6c8ae10c19642837280b445d2
MattCobosco <mateuszkalucki@gmail.com>
2022-05-28 17:40:20 +0200

    Authenticator

---
+++ StockTrader.WPF/State/Authenticators/Authenticator.cs
@@ -0,0 +1,47 @@
+﻿using StockTrader.Domain.Models;
+using StockTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader.WPF.State.Authenticators
+{
+    public class Authenticator : IAuthenticator
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public Authenticator(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        public Account CurrentAccount { get; private set; }
+
+        public bool IsLoggedIn => CurrentAccount != null;
+
+        public async Task<bool> Login(string username, string password)
+        {
+            try
+            {
+                CurrentAccount = await _authenticationService.Login(username, password);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Logout()
+        {
+            CurrentAccount = null;
+        }
+
+        public Task<RegistrationResult> Register(string email, string username, string password, string confirmPassword)
+        {
+            return _authenticationService.Register(email, username, password, confirmPassword);
+        }
+    }
+}
