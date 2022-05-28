7b48d1faed3c5dc712762c4df3333a233cce5202
MattCobosco <mateuszkalucki@gmail.com>
2022-05-28 14:31:04 +0200

    Replaced Password with PasswordHash in User model

--- StockTrader.Domain/Models/User.cs
+++ StockTrader.Domain/Models/User.cs
@@ -4,8 +4,7 @@
     {
         public string Email { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
         public DateTime DateJoined { get; set; }
-
     }
 }
---
+++ StockTrader.EntityFramework/Migrations/20220528122912_UserPasswordToPasswordHash.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockTrader.EntityFramework.Migrations
+{
+    public partial class UserPasswordToPasswordHash : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Password");
+        }
+    }
+}
