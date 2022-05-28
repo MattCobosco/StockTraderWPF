80a2221538e1172cf6112b69a91b6543d50bac90
MattCobosco <mateuszkalucki@gmail.com>
2022-05-28 15:02:50 +0200

    IAccountService extends IDataService<Account>

---
+++ StockTrader.Domain/Services/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services
+{
+    public interface IAccountService : IDataService<Account>
+    {
+        Task<Account> GetByUsername(string username);
+        Task<Account> GetByEmail(string email);
+    }
+}
