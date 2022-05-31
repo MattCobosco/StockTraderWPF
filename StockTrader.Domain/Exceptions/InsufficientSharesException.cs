58ad8738c1835c28f0b80f13ec087adff9bf1139
MattCobosco <mateuszkalucki@gmail.com>
2022-06-01 00:29:37 +0200

    Exception when user tries to sell more shares than they own

---
+++ StockTrader.Domain/Exceptions/InsufficientSharesException.cs
@@ -0,0 +1,25 @@
+﻿namespace StockTrader.Domain.Exceptions
+{
+    public class InsufficientSharesException : Exception
+    {
+        public int ActualShares { get; set; }
+        public int RequiredShares { get; set; }
+
+        public InsufficientSharesException(int actualShares, int requiredShares)
+        {
+            ActualShares = actualShares;
+            RequiredShares = requiredShares;
+        }
+        public InsufficientSharesException(int actualShares, int requiredShares, string? message) : base(message)
+        {
+            ActualShares = actualShares;
+            RequiredShares = requiredShares;
+        }
+
+        public InsufficientSharesException(int acutalShares, int requiredShares, string? message, Exception? innerException) : base(message, innerException)
+        {
+            ActualShares = acutalShares;
+            RequiredShares = requiredShares;
+        }
+    }
+}
