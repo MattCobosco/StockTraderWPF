1be060ba8c7b554eb7fe2fb3059bbf4ec911efe1
MattCobosco <mateuszkalucki@gmail.com>
2022-06-01 00:17:21 +0200

    comment moved to interface

--- StockTrader.Domain/Services/TransactionServices/ISellStockService.cs
+++ StockTrader.Domain/Services/TransactionServices/ISellStockService.cs
@@ -4,6 +4,15 @@ namespace StockTrader.Domain.Services.TransactionServices
 {
     public interface ISellStockService
     {
+        /// <summary>
+        /// Sell a stock for an account.
+        /// </summary>
+        /// <param name="seller">The account of the seller.</param>
+        /// <param name="stockSymbol">The symbol of a stock sold.</param>
+        /// <param name="shareAmount">The number of shares to sell.</param>
+        /// <returns>The updated account after the sell transaction.</returns>
+        /// <exception cref="InvalidSymbolException">Thrown if the sold symbol is invalid.</exception>
+        /// <exception cref="Exception">Thrown if the sell transaction fails.</exception>
         Task<Account> SellStock(Account seller, string stockSymbol, int shareAmount);
     }
 }
