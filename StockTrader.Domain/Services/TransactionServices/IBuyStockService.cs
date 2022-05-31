3931ec89007f157d82016631648fa1b7d6e61312
MattCobosco <mateuszkalucki@gmail.com>
2022-05-31 23:11:37 +0200

    comments

--- StockTrader.Domain/Services/TransactionServices/IBuyStockService.cs
+++ StockTrader.Domain/Services/TransactionServices/IBuyStockService.cs
@@ -1,4 +1,5 @@
 ﻿using StockTrader.Domain.Models;
+using StockTrader.Domain.Exceptions;
 
 namespace StockTrader.Domain.Services.TransactionServices
 {
@@ -12,6 +13,8 @@ namespace StockTrader.Domain.Services.TransactionServices
         /// <param name="shareAmount">The number of shares to buy.</param>
         /// <returns>The updated account after the buy transaction.</returns>
         /// <exception cref="InsufficientFundsException">Thrown if the account has insufficient balance.</exception>
+        /// <exception cref="InvalidSymbolException">Thrown if the purchased symbol does not exist.</exception>
+        /// <exception cref="Exception">Thrown if the buy transaction fails.</exception>
         Task<Account> BuyStock(Account buyer, string stockSymbol, int shareAmount);
     }
 }
