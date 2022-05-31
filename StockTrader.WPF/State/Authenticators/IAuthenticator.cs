6a4802df7176a90e83aacf821172e775611727fd
MattCobosco <mateuszkalucki@gmail.com>
2022-05-31 23:48:12 +0200

    commands for login

--- StockTrader.WPF/State/Authenticators/IAuthenticator.cs
+++ StockTrader.WPF/State/Authenticators/IAuthenticator.cs
@@ -1,5 +1,6 @@
 ﻿using StockTrader.Domain.Models;
 using StockTrader.Domain.Services;
+using StockTrader.Domain.Exceptions;
 using System;
 using System.Threading.Tasks;
 
@@ -12,6 +13,15 @@ namespace StockTrader.WPF.State.Authenticators
         event Action StateChanged;
 
         Task<RegistrationResult> Register(string email, string username, string password, string confirmPassword);
+        
+        /// <summary>
+        /// Login to the app.
+        /// </summary>
+        /// <param name="username">User's name.</param>
+        /// <param name="password">User's password.</param>
+        /// <exception cref="UserNotFoundException">Thrown when username does not exist in the database.</exception>
+        /// <exception cref="InvalidPasswordException">Thrown when the provided password is incorrect.</exception>
+        /// <exception cref="Exception">Thrown if the login fails.</exception>
         Task Login(string username, string password);
         void Logout();
     }
