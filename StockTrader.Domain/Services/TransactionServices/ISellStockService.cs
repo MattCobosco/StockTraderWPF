cddbb9592a8ed76cad6101dedcd4ac54aa717822
MattCobosco <mateuszkalucki@gmail.com>
2022-05-31 22:34:06 +0200

    SellStockService init

---
+++ StockTrader.Domain/Services/TransactionServices/ISellStockService.cs
@@ -0,0 +1,9 @@
+﻿using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services.TransactionServices
+{
+    public interface ISellStockService
+    {
+        Task<Account> SellStock(Account seller, string stockSymbol, int shareAmount);
+    }
+}
---
+++ StockTrader.Domain/Services/TransactionServices/SellStockService.cs
@@ -0,0 +1,19 @@
+﻿using StockTrader.Domain.Models;
+
+namespace StockTrader.Domain.Services.TransactionServices
+{
+    public class SellStockService : ISellStockService
+    {
+        /// <summary>
+        /// Sell a stock for an account.
+        /// </summary>
+        /// <param name="seller">The account of the seller.</param>
+        /// <param name="stockSymbol">The symbol of a stock sold.</param>
+        /// <param name="shareAmount">The number of shares to sell.</param>
+        /// <returns>The updated account after the sell transaction.</returns>
+        public Task<Account> SellStock(Account seller, string stockSymbol, int shareAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
