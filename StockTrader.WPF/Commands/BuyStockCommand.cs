379f3fda3c6328afc360d85d95557fef0a02b525
MattCobosco <mateuszkalucki@gmail.com>
2022-05-31 23:20:53 +0200

    clear error/status messages, better status string grammar

--- StockTrader.WPF/Commands/BuyStockCommand.cs
+++ StockTrader.WPF/Commands/BuyStockCommand.cs
@@ -30,14 +30,19 @@ namespace StockTrader.WPF.Commands
         
         public async void Execute(object? parameter)
         {
+            _buyViewModel.StatusMessage = string.Empty;
+            _buyViewModel.ErrorMessage = string.Empty;
+                
             try
             {
                 string symbol = _buyViewModel.Symbol;
                 int stockAmountToBuy = _buyViewModel.StockAmountToBuy;
+                string shareOrShares = stockAmountToBuy == 1 ? "share" : "shares";
+
                 Account account = await _buyStockService.BuyStock(_accountStore.CurrentAccount, symbol, stockAmountToBuy);
                 _accountStore.CurrentAccount = account;
 
-               _buyViewModel.StatusMessage = $"{stockAmountToBuy} shares of {symbol} bought successfully.";
+               _buyViewModel.StatusMessage = $"{stockAmountToBuy} {shareOrShares} of {symbol} bought successfully.";
             }
             catch (InsufficientFundsException)
             {
