b827c4089d2a95e9b878834593aca6f60bd1538d
MattCobosco <mateuszkalucki@gmail.com>
2022-06-01 00:30:49 +0200

    Added property for Symbol

--- StockTrader.Domain/Exceptions/InsufficientSharesException.cs
+++ StockTrader.Domain/Exceptions/InsufficientSharesException.cs
@@ -2,22 +2,26 @@
 {
     public class InsufficientSharesException : Exception
     {
+        public string Symbol { get; set; }
         public int ActualShares { get; set; }
         public int RequiredShares { get; set; }
 
-        public InsufficientSharesException(int actualShares, int requiredShares)
+        public InsufficientSharesException(string symbol, int actualShares, int requiredShares)
         {
+            Symbol = symbol;
             ActualShares = actualShares;
             RequiredShares = requiredShares;
         }
-        public InsufficientSharesException(int actualShares, int requiredShares, string? message) : base(message)
+        public InsufficientSharesException(string symbol, int actualShares, int requiredShares, string? message) : base(message)
         {
+            Symbol = symbol;
             ActualShares = actualShares;
             RequiredShares = requiredShares;
         }
 
-        public InsufficientSharesException(int acutalShares, int requiredShares, string? message, Exception? innerException) : base(message, innerException)
+        public InsufficientSharesException(string symbol, int acutalShares, int requiredShares, string? message, Exception? innerException) : base(message, innerException)
         {
+            Symbol = symbol;
             ActualShares = acutalShares;
             RequiredShares = requiredShares;
         }
